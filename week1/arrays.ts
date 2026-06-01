d4ca8094e058387cfe5289d4751bc95baaa08f51
YVGK <xyvgenix29@gmail.com>
2026-06-01 22:09:20 +0300

    week1: array and string katas (evenNumbers, sumArray, reverseString)

---
+++ week1/arrays.ts
@@ -0,0 +1,30 @@
+function evenNumbers(arr: number[]): number[] {
+  let results: number[] = [];
+  for (let n of arr) {
+    if (n % 2 == 0) results.push(n);
+  }
+  return results;
+}
+
+function sumArray(arr: number[]): number {
+  let result: number = 0;
+  for (let n of arr) {
+    result = result + n;
+  }
+  return result;
+}
+
+function reverseString(s: string): string {
+  let result: string = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    result = result + s.charAt(i);
+  }
+  return result;
+}
+
+const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arraySum: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const word: string = "Hello World";
+console.log("Function evenNumbers, array [1,2,3,4,5,6,7,8,9]: ", evenNumbers(array));
+console.log("Function sumArray, arraySum [1,2,3,4,5,6,7,8,9]: ", sumArray(arraySum));
+console.log("Function reverseString, String Hello World: ", reverseString(word));

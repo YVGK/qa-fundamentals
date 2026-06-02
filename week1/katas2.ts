d1e048a9caf85542372ca91fd3ba53f87957e17c
YVGK <xyvgenix29@gmail.com>
2026-06-02 20:49:13 +0300

    Week1, function countWords for counting the number of words in a string, function findMax to find the max number in array, including fallback for empty array with return on MIN_VALUE (closest to zero)

--- week1/katas2.ts
+++ week1/katas2.ts
@@ -1,28 +1,27 @@
 function countWords(s: string): number {
   let total: number = 0;
-  const trimedString: string = s.replace(/\s+/g, "");
-  for (const n of trimedString) {
-    total = total + 1;
-    console.log(n, total);
+  const myWords: string[] = s.split(" ");
+  for (const n of myWords) {
+    if (n.length > 1) {
+      total = total + 1;
+    }
   }
   return total;
 }
 
-console.log(countWords("Hello   world  "));
-
 function findMax(arr: number[]): number {
-  let maxNumber: number = 0;
+  let maxNumber: number = Number.MIN_SAFE_INTEGER;
   if (arr.length > 0) {
     for (const n of arr) {
-      if (n > maxNumber) {
+      if (n >= maxNumber) {
         maxNumber = n;
       }
     }
+  } else {
+    console.log("Array is empty");
+    maxNumber = Number.MIN_VALUE;
+    // returns MIN_VALUE;
   }
 
   return maxNumber;
 }
-const array: number[] = [1, 5, 8, 3, 1, 6, 2];
-const array2: number[] = [];
-console.log("The max Number is ", findMax(array));
-console.log("The max Number is ", findMax(array2));

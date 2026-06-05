b60dff4dce7f9dddd2bbd410dd478421150280c5
YVGK <xyvgenix29@gmail.com>
2026-06-05 13:07:29 +0300

    week1: fix countWords one-letter words + findMax negatives/empty

--- week1/katas2.ts
+++ week1/katas2.ts
@@ -2,7 +2,8 @@ function countWords(s: string): number {
   let total: number = 0;
   const myWords: string[] = s.split(" ");
   for (const n of myWords) {
-    if (n.length > 1) {
+    // bigger then 0 due to words like I , A
+    if (n.length > 0) {
       total = total + 1;
     }
   }
@@ -18,10 +19,17 @@ function findMax(arr: number[]): number {
       }
     }
   } else {
-    console.log("Array is empty");
-    maxNumber = Number.MIN_VALUE;
-    // returns MIN_VALUE;
+    // returns infinty beacuse throw error is stopping the program
+    maxNumber = Infinity;
   }
-
   return maxNumber;
 }
+const sentence: string = "I am A Automation engineer ";
+const normalArray: number[] = [2, 6, 1, 2, 8, 5, 4, 7];
+const emptyArray: number[] = [];
+const negativeArray: number[] = [-5, -2, -6, -8, -1, -6];
+
+console.log(`String '${sentence}' has ${countWords(sentence)} words`);
+console.log(`The biggest number in normalArray is ${findMax(normalArray)}`);
+console.log(`The biggest number in emptyArray is ${findMax(emptyArray)}`);
+console.log(`The biggest number in negativeArray is ${findMax(negativeArray)}`);
